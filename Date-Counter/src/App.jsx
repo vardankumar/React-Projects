26da5b7b1800f53dff947f7aa6bb566cb5e8a5d0
Verdan Kumar <vardantharwani@gmail.com>
2024-03-01 18:47:56 +0500

    added Date Counter mini app

--- Date-Counter/src/App.jsx
+++ Date-Counter/src/App.jsx
@@ -1,13 +1,48 @@
+import { useState } from "react"
 
-import './App.css'
-
-function App() {
-
+const App = () => {
   return (
-    <>
-
-    </>
+    <div className="App">
+      <Counter/>
+    </div>
   )
 }
+
+function Counter(){
+
+  let [count, setCount] = useState(0)
+  let [step, setStep] = useState(1)
+
+  const date = new Date()
+  date.setDate(date.getDate() + count)
+
+  return(
+    <>
+      <div>
+
+        <button onClick={() => setStep((step) => step - 1)}>-</button>
+        <span>Step : {step}</span>
+        <button onClick={() => setStep((step) => step + 1)}>+</button>
+
+      </div>
+
+      <div>
+
+          <button onClick={() => setCount((count) => count - step)}>-</button>
+          <span>Count : {count}</span>
+          <button onClick={() => setCount((count) => count + step)}>+</button>
+
+      </div>
+
+      <p>
+        <span>{count === 0 ? "Today is " : count > 0 ? `${count} days from today is ` : `${Math.abs(count)} days ago was `}</span>
+        <span>{date.toDateString()}</span>
+      </p>
+
+  </>
+  )
+}
+
+
 
 export default App
