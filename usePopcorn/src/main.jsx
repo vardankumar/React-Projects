5cf63f09bba2ea3747aeeb10752e7a7b15b4f17b
Verdan Kumar <vardantharwani@gmail.com>
2024-03-28 02:22:43 +0500

    Adding a few features and code updates

--- usePopcorn/src/main.jsx
+++ usePopcorn/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
+import StarRating from './StarRating'
+import App from './App-v1'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <StarRating maxRating={5} />
+    <StarRating maxRating={5} size={24} color='red' /> */}
+    <App/>
   </React.StrictMode>,
 )
