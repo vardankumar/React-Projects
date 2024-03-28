c5e444a8e391b639530d29ec826bc5035dc2028a
Verdan Kumar <vardantharwani@gmail.com>
2024-03-29 04:45:41 +0500

    Adding react quiz app and updating the dateCounter component with useReducer hook for practice

---
+++ Quiz App/src/App.jsx
@@ -0,0 +1,12 @@
+import DateCounter from './DateCounter'
+
+function App() {
+
+  return (
+    <>
+      <DateCounter/>
+    </>
+  )
+}
+
+export default App
---
+++ Quiz App/src/DateCounter.jsx
@@ -0,0 +1,111 @@
+import { useReducer } from "react";
+
+
+const reducer = (state, action) => {
+  // if(action.type === "dec"){
+  //   return state - +action.payload
+  // }
+  // if(action.type === "inc"){
+  //   return state + +action.payload
+  // }
+  // if(action.type === "setCount"){
+  //   return +action.payload
+  // }
+
+  switch(action.type){
+    case 'dec':
+      return {...state, count : state.count - state.step}
+    case 'inc':
+      return {...state, count : state.count + state.step}
+    case 'setCount':
+      return {...state, count : +action.payload}
+    case 'setStep':
+      return {...state, step : +action.payload}
+    case 'reset':
+      return {count : 0, step : 1}
+
+    default:
+      throw new Error("Unknown action")
+  }
+
+
+
+}
+
+function DateCounter() {
+
+  const initialState = {
+    count: 0,
+    step: 1
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { count, step } = state
+
+
+  // This mutates the date object.
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + state.count);
+
+  const dec = function () {
+    dispatch({
+      type : 'dec',
+      payload : 1
+    })
+  };
+  
+  const inc = function () {
+    dispatch({
+      type : 'inc',
+      payload : 1
+    })
+  };
+
+  const defineCount = function (e) {
+    dispatch({
+      type : "setCount",
+      payload : e.target.value
+    })
+  };
+
+  const defineStep = function (e) {
+    dispatch({
+      type : "setStep",
+      payload : +e.target.value
+    })
+  };
+
+  const reset = function () {
+    dispatch({
+      type : "reset",
+    })
+  };
+
+  return (
+    <div className="counter">
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={defineStep}
+        />
+        <span>{step}</span>
+      </div>
+
+      <div>
+        <button onClick={dec}>-</button>
+        <input value={count} onChange={defineCount} />
+        <button onClick={inc}>+</button>
+      </div>
+
+      <p>{date.toDateString()}</p>
+
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+export default DateCounter;
